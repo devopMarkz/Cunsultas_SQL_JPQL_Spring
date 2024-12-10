537f4cca36d144d7fd80ef2e031ab4487dffe9a9
devopMarkz <marcosdev2002@gmail.com>
2024-12-10 20:23:21 -0300

    Criando consulta personalizada com SQL puro

--- uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
+++ uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -1,12 +1,27 @@
 package com.devsuperior.uri2611;
 
+import com.devsuperior.uri2611.projections.MovieProjection;
+import com.devsuperior.uri2611.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class Uri2611Application {
+public class Uri2611Application implements CommandLineRunner {
+
+	@Autowired
+	private MovieRepository movieRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2611Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<MovieProjection> testSearch1 = movieRepository.search1("Action");
+		testSearch1.forEach(movieProjection -> System.out.println(movieProjection.getId() + ", " + movieProjection.getName()));
 	}
 }
