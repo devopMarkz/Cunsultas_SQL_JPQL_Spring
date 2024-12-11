18edf0e915806788f55c9d8d1595eb6ef4db498a
devopMarkz <marcosdev2002@gmail.com>
2024-12-11 08:10:23 -0300

    Criando consultas personalizadas em SQL e JPQL com group by através da annotation @Query

--- uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,11 +1,15 @@
 package com.devsuperior.uri2609;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Uri2609Application implements CommandLineRunner {
@@ -19,6 +23,10 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		List<CategorySumProjection> testSearch1 = repository.search1();
+		testSearch1.forEach(categorySumProjection -> System.out.println(categorySumProjection.getName() + ", " + categorySumProjection.getSum()));
 
+		List<CategorySumDTO> testSearch2 = repository.testSearch2();
+		testSearch2.forEach(categorySumProjection -> System.out.println(categorySumProjection.getName() + ", " + categorySumProjection.getSum()));
 	}
 }
