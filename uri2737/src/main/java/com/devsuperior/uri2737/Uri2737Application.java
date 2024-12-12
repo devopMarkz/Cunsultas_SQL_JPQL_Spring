9bfba40961efc2e90e32255713daef97be951aa0
devopMarkz <marcosdev2002@gmail.com>
2024-12-12 13:40:27 -0300

    Criando consultas personalizadas de união

--- uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,11 +1,14 @@
 package com.devsuperior.uri2737;
 
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2737.repositories.LawyerRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Uri2737Application implements CommandLineRunner {
@@ -19,7 +22,7 @@ public class Uri2737Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-
+		List<LawyerMinProjection> testSearch1 = repository.search1();
+		testSearch1.forEach(l-> System.out.println(l.getName() + ", " + l.getCustomersNumber()));
 	}
 }
