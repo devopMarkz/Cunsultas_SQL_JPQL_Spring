d11a51ef4b9d8c428e31494fd67415ecd013cc4e
devopMarkz <marcosdev2002@gmail.com>
2024-12-14 13:59:12 -0300

    Puxando empregados que não trabalham em nenhum projeto com subconsulta

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,11 +1,14 @@
 package com.devsuperior.uri2990;
 
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Uri2990Application implements CommandLineRunner {
@@ -19,6 +22,7 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		List<EmpregadoDeptProjection> list1 = repository.search1();
+		list1.forEach(e -> System.out.println(e.getCpf() + ", " + e.getEnome() + ", " + e.getDnome()));
 	}
 }
